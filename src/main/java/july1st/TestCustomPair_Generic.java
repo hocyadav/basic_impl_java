f037bc0bd1ae102921cd8045581ebc11d5d4c38d
Hariom Yadav <hoc.yadav@gmail.com>
2020-07-01 07:54:58 -0700

    pair hide public contructor to make same as pair

--- src/main/java/july1st/TestCustomPair_Generic.java
+++ src/main/java/july1st/TestCustomPair_Generic.java
@@ -2,11 +2,19 @@ package july1st;
 
 public class TestCustomPair_Generic {
 	public static void main(String[] args) {
-		CustomPair_Generic<Integer, String> pr = new CustomPair_Generic<Integer, String>();
+		//initial fixed value
+		CustomPair_Generic2<Integer, String> pr = new CustomPair_Generic2<Integer, String>(12,"omprakash");
+		System.out.println("initail		:"+pr);//print initial value
 		pr.setKey(1);
 		pr.setVal("hariom");
-		System.out.println(pr);
-		System.out.println("key :"+pr.getKey());
-		System.out.println("val :"+pr.getVal());
+		System.out.println("new 		:"+pr);//print final value
+
+		
+		CustomPair_Generic2<String, Integer> pr2 = new CustomPair_Generic2<String, Integer>("chandan", 1233);
+		System.out.println("initail 	:"+pr2);//print initial value
+		pr2.setKey("hariom yadav");
+		pr2.setVal(123);
+		System.out.println("new 		:"+pr2);//print final value
+		
 	}
 }
