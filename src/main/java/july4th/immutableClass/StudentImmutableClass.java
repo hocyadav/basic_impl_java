70f4b3967817d2ead00934c6ff4d6915f94d92e9
Hariom Yadav <hoc.yadav@gmail.com>
2020-07-04 22:36:52 +0530

    comment added

--- src/main/java/july4th/immutableClass/StudentImmutableClass.java
+++ src/main/java/july4th/immutableClass/StudentImmutableClass.java
@@ -1,7 +1,7 @@
 package july4th.immutableClass;
-//1. make class final
-//2. make field private + final
-//3  only getter, no setter
+//1. make class final : else any one can extend and probality that value can be change by child class
+//2. make field private + final : else own class obj can change its value , obj.id = 12
+//3  only getter, no setter : else by using setter anyone can change value
 public final class StudentImmutableClass {
 	private final int id;
 	private final String name;
