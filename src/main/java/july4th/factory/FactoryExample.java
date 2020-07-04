448648ebad6cec59e977c2f31a95ac8e427719f7
Hariom Yadav <hoc.yadav@gmail.com>
2020-07-04 21:20:00 +0530

    comment

--- src/main/java/july4th/factory/FactoryExample.java
+++ src/main/java/july4th/factory/FactoryExample.java
@@ -2,7 +2,7 @@ package july4th.factory;
 
 public class FactoryExample {
 	public static void main(String[] args) {
-		Vehicle obj1 = VehicleFactory.getInstance("car", 4);
+		Vehicle obj1 = VehicleFactory.getInstance("car", 4);//send string type that we want + values that object creating required as 2nd argument
 		System.out.println(obj1);
 		
 		Vehicle obj2 = VehicleFactory.getInstance("bike", 2);
@@ -10,8 +10,5 @@ public class FactoryExample {
 		
 		Vehicle obj3 = VehicleFactory.getInstance("askjhdakjsd", 2);
 		System.out.println(obj3);
-		
-		
-		
 	}
 }
