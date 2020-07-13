840c33200dd044c98270ec4a757f3c6302587339
Hariom Yadav <hoc.yadav@gmail.com>
2020-07-13 18:26:08 +0530

    localdate time for machine coding

--- src/main/java/july13th/LocalDateTime_Ex.java
+++ src/main/java/july13th/LocalDateTime_Ex.java
@@ -1,13 +1,31 @@
 package july13th;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
 
+/** Local Date time implementation for machine coding **/
 public class LocalDateTime_Ex {
+	
 	public static void main(String[] args) {
 		LocalDateTime localDt = LocalDateTime.now();
 		System.out.println(localDt);
 		
 		splitNMerge(localDt);
+		
+		//use this one in system design
+		String format = localDt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));//good
+		System.out.println(format);
+		
+		String format3 = localDt//input localdatetime object 
+							.plusDays(2) //add days
+							.plusHours(1)//add time..its like lambda
+							.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.MEDIUM));//good
+		System.out.println(format3);
+		
+		
+		String format2 = localDt.format(DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT));
+		System.out.println(format2);
 	}
 
 	//input : 2020-07-13T18:14:50.362 --> output : 2020-07-13 18:14:50.362
@@ -19,6 +37,15 @@ public class LocalDateTime_Ex {
 			System.out.println(str);
 		}
 		String join = String.join(" ", split);
-//		System.out.println(join);/2020-07-13 18:14:50.362
+		System.out.println(join);//2020-07-13 18:14:50.362
 	}
 }
+
+/*
+2020-07-13T18:23:54.808
+2020-07-13
+18:23:54.808
+Jul 13, 2020 6:23:54 PM
+Jul 15, 2020 7:23:54 PM
+7/13/20 6:23 PM
+ */
