4ecde0022570ad99ed7d0f166a973c9fde620555
Hariom Yadav <hoc.yadav@gmail.com>
2020-07-13 17:56:23 +0530

    local date example

---
+++ src/main/java/july13th/LocalDate_Example.java
@@ -0,0 +1,25 @@
+package july13th;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+public class LocalDate_Example {
+	public static void main(String[] args) {
+		LocalDate localDate = LocalDate.now();
+		System.out.println(localDate);
+		
+		LocalDate localDateParse = LocalDate.parse("1989-06-26");
+		System.out.println(localDateParse);
+		DayOfWeek ob = localDateParse.getDayOfWeek();
+		System.out.println("my birth day : "+ob);
+
+		
+		boolean before = localDateParse.isBefore(localDate);
+		System.out.println(before);
+		
+		LocalDate myBirthNextDay = localDateParse.plus(1, ChronoUnit.DAYS);
+		System.out.println(myBirthNextDay);
+		
+	}
+}
